716b41d3400c872a641fe09a4d6fc61dfde7b498
bryanjamesdelaluya <delaluya@gmail.com>
2022-07-24 14:37:15 +0800

    initial commit

---
+++ config/constants.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "placeholder_image" => "public/images/placeholders/placeholder.jpg",
+    "no_reply_email" => "delaluya+no-reply@gmail.com",
+    "bjcdl_bcc" => "delaluya@gmail.com",
+];
---
+++ resources/views/admin/layouts/sections/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<footer class="clearfix">
+    <div class="pull-right">
+        {{--Crafted with <i class="fa fa-heart text-danger"></i> by--}}
+        <a href="https://bjcdl.github.io/" target="_blank">BJCDL</a> &reg;
+    </div>
+    <div class="pull-left">
+        <span id="year-copy"></span> &copy; <a href="javascript:void(0)">{!! $seo_meta['name'] . ' ' . $admin_template['version'] !!}</a>
+    </div>
+</footer>
